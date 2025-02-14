457cb148afdc71922d3d5127b9dde4c402b0ac44
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-14 13:01:47 +0100

    home

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import "./App.css";
 import TopBar from "./components/TopBar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Weather from "./components/Weather";
-import Home from "./components/Home";
+import Weather from "./components/WeatherHome";
 
 const App = () => (
   <BrowserRouter>
     <TopBar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/weather" element={<Weather />} />
+      <Route path="/" element={<Weather />} />
+      {/*  <Route path="/weather" element={<5Days />} /> */}
       {/* <Route path="/weather/5-days" element={<MovieDetails />} /> */}
     </Routes>
   </BrowserRouter>
